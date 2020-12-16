6361ac863de0a11e0dcc94f1939eb9a4b3059036
Sean Jungbluth <jungbluth.sean@gmail.com>
2020-12-16 15:53:33 -0800

    Dev

--- lib/ncbi.py
+++ lib/ncbi.py
@@ -60,6 +60,7 @@ def fetch_taxonomy_name_from_assembly_id(assembly_id_list, size):
         else:
           current_size = int(math.floor(current_size/2))
           input_list = input_list[:current_size]
+          print("portion appeneded {}".format(str(input_list[current_size:])))
           remaining_list = input_list[current_size:] + remaining_list
           print("IDs lost because of redundant results, retrying with list size: {}".format(current_size))
     else:
