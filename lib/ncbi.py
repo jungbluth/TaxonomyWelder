d4babe4d15c38dfeb1e18b4b1547c6299d805059
Sean Jungbluth <jungbluth.sean@gmail.com>
2020-12-16 12:06:27 -0800

    Dev

--- lib/ncbi.py
+++ lib/ncbi.py
@@ -2,6 +2,7 @@
 
 import random
 import subprocess
+import math
 import pandas as pd
 
 
@@ -31,13 +32,39 @@ def fetch_taxonomy_name_from_assembly_id(assembly_id_list, size):
   
   # shuffle list because otherwise identical entires collapsed, want to minimize
   print("Fetching NCBI TaxIDs from GTDB-used NCBI Assembly IDs")
-  print("To number of IDs to link: {}".format(len(assembly_id_list)))
+  print("Number of IDs to link: {}".format(len(assembly_id_list)))
   random.shuffle(assembly_id_list)
-  if (len(assembly_id_list) != 0) and (len(assembly_id_list) > size):
-    input_list = assembly_id_list[:size]
-    remaining_list = assembly_id_list[size:]
-    print("input_list is {} ".format(input_list))
-    with open("list.txt", "w") as output:
-      output.write('\n'.join((input_list)))
-    command = "epost -input list.txt -db assembly | elink -target taxonomy | efetch -format uid > temp"
-    out, err = _run_command(command)
+  current_size = size
+  all_matched = False
+  output_assembly_id_list = []
+  output_matched_ncbi_taxid_list = []
+  while len(output_assembly_id_list) != len(assembly_id_list):
+    while not all_matched:
+      print("Linking IDs in chunks of {}".format(current_size))
+      if (len(assembly_id_list) != 0) and (len(assembly_id_list) > current_size):
+        input_list = assembly_id_list[:current_size]
+        remaining_list = assembly_id_list[current_size:]
+        print("input_list is {} ".format(input_list))
+        with open("list.txt", "w") as output:
+          output.write('\n'.join((input_list)))
+        command = "epost -input list.txt -db assembly | elink -target taxonomy | efetch -format uid > temp"
+        out, err = _run_command(command)
+        count = 0
+        with open('temp', 'r') as f:
+          lines = [line.rstrip() for line in f]
+          for line in f:
+            count += 1
+        if count == current_size:
+          print("All IDs matched")
+          output_assembly_id_list.append(input_list)
+          output_matched_ncbi_taxid_list.append(lines)
+          all_matched = True
+        else:
+          current_size = int(math.floor(current_size/2))
+          print("IDs lost because of redundant results, retrying with list size: {}".format(current_size))
+
+
+
+
+
+
