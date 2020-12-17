d07441ee74bb3a1f7fc0da71c4ac85cc1a3c1a02
Sean Jungbluth <jungbluth.sean@gmail.com>
2020-12-16 21:04:57 -0800

    Dev

--- lib/ncbi.py
+++ lib/ncbi.py
@@ -52,10 +52,10 @@ def fetch_taxonomy_name_from_assembly_id_single(assembly_id_list):
       print("\nTotal number of IDs matched: {} (out of {}) ({} percent done)".format(len(output_assembly_id_list),len(assembly_id_list),percent_done))
       input_list = remaining_list[:1]
       remaining_list = remaining_list[1:]
-      with open('output_incremental.txt', 'a') as f:
+      with open('/tmp/output_incremental.txt', 'a') as f:
         f.write("{} {}\n".format(output_assembly_id_list[-1], output_matched_ncbi_taxid_list[-1]))
       continue
-  with open('output_all.txt', 'a') as f:
+  with open('/tmp/output_all.txt', 'a') as f:
     for i in range(len(output_assembly_id_list)):
       f.write("{} {}\n".format(output_assembly_id_list[i], output_matched_ncbi_taxid_list[i]))
 
