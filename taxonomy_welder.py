8cb78aaa0e76f108553cb21d4402dcc9b6073bd8
Sean Jungbluth <jungbluth.sean@gmail.com>
2021-01-19 10:54:37 -0800

    Added IMG tables

--- taxonomy_welder.py
+++ taxonomy_welder.py
@@ -2,6 +2,9 @@
 
 import csv
 import random
+import sys
+import argparse
+from argparse import RawTextHelpFormatter
 from lib import gtdb, silva
 import datetime
 
@@ -34,20 +37,34 @@ def database_versions():
   print("GTDB - {}".format(gtdb_version))
   #print("NCBI - {}\n".format(ncbi_version))
 
+def arg_parser():
+  parser = argparse.ArgumentParser(prog='taxonomy_welder',usage='%(prog)s.py --dl_gtdb [y/n] --dl_silva [y/n] --help --version', description="""
+  taxonomy_welder is a toolkit for query/parsing/merging NCBI/GTDB/SILVA taxonomies.
+  """,formatter_class=RawTextHelpFormatter)
+  parser.add_argument("--dl_gtdb", dest="dl_gtdb", default="n", help="""Download GTDB-based files. (default n)""")
+  parser.add_argument("--dl_silva", dest="dl_silva", default="n", help="""Download SILVA-based files. (default n)""")
+  parser.add_argument('--version', action='version', version='%(prog)s v0.0.1')
+  args = parser.parse_args()
+  if len(sys.argv) is None:
+    parser.print_help()
+  return args
 
 if __name__ == "__main__":
 
   welcome_message()
 
+  args = arg_parser()
+
   database_versions()
 
-  # step 1) import gtdb data
-  df_gtdb_to_ncbi_to_gg_silva_archaea_plus_bacteria_trim = gtdb.import_gtdb_to_ncbi_to_gg_to_silva_table()
+  if args.dl_gtdb is not 'n':
+    print("Start: Downloading GTDB files")
+    df_gtdb_to_ncbi_to_gg_silva_archaea_plus_bacteria_trim = gtdb.import_gtdb_to_ncbi_to_gg_to_silva_table()
+    df_gtdb_to_ncbi_to_gg_silva_archaea_plus_bacteria_trim.to_csv("df_gtdb_to_ncbi_to_gg_silva_archaea_plus_bacteria_trim.tsv", index=False, sep='\t', header=True)
+    print("End: Downloading GTDB files")
 
-  # step 2) import silva data
-  df_silva_to_ncbi = silva.import_silva_to_ncbi_table()
-
-  # step 3) write tables
-  df_gtdb_to_ncbi_to_gg_silva_archaea_plus_bacteria_trim.to_csv("df_gtdb_to_ncbi_to_gg_silva_archaea_plus_bacteria_trim.tsv", index=False, sep='\t', header=True)
-  df_silva_to_ncbi.to_csv("df_silva_to_ncbi.tsv", index=False, sep='\t', header=True)
-
+  if args.dl_silva is not 'n':
+    print("Start: Downloading SILVA files")
+    df_silva_to_ncbi = silva.import_silva_to_ncbi_table()
+    df_silva_to_ncbi.to_csv("df_silva_to_ncbi.tsv", index=False, sep='\t', header=True)
+    print("End: Downloading SILVA files")
